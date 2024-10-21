6a2cb2ef70621ffd398bc8a53c42e201792e0408
Sabba Perween <sabbaperween14@gmail.com>
2024-10-21 16:38:22 +0530

    Eda and Problem statment

--- src/exception.py
+++ src/exception.py
@@ -19,11 +19,11 @@ class CustomException(Exception):
         return self.error_massage
     
     
-if __name__=="__main__":
-    try:
-        a=1/0
-    except Exception as e:
-        logging.info("Divide by Zero")
-        raise CustomException(e,sys)
+# if __name__=="__main__":
+#     try:
+#         a=1/0
+#     except Exception as e:
+#         logging.info("Divide by Zero")
+#         raise CustomException(e,sys)
     
     
